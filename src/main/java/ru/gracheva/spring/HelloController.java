bebf4a5343243f1daa4cd25c0e32b89ec3ddfd6b
n.gracheva <gracheva_22_09@mail.ru>
2022-01-04 18:32:42 +0500

    initial commit

---
+++ src/main/java/ru/gracheva/spring/HelloController.java
@@ -0,0 +1,13 @@
+package ru.gracheva.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-spring")
+    public String sayHello() {
+        return "hello_spring";
+    }
+}
